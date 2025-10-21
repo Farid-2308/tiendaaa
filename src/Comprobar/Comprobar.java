55f525477cf6c3b3273015f42d4a12d0cbd80ca6
Victor <duvanlopez1407@gmail.com>
2025-10-20 19:18:15 -0600

    Comprobar
    Comprobar si, si podemos trabajar junto

---
+++ src/Comprobar/Comprobar.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Comprobar;
+
+/**
+ *
+ * @author Student
+ */
+public class Comprobar {
+    
+}
