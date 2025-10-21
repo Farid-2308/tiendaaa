33b76a912d206c95613ca6d02a7a2e7cf222db01
Victor <duvanlopez1407@gmail.com>
2025-10-20 20:37:23 -0600

    Factory Method
    Se creo una clase de ProductoFactory para que el patrón de diseño funcione exitosamente

--- src/Catalogo/ProductoFactory.java
+++ src/Catalogo/ProductoFactory.java
@@ -2,12 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Comprobar;
+package Catalogo;
 
 /**
  *
  * @author Student
  */
-public class Comprobar {
+public class ProductoFactory {
+
+    public Producto CrearProducto(String codigo, String nombre, double precio, int stock, Categoria categoria) {
+        return new Producto(codigo,nombre,precio,stock,categoria);
+    }
     
 }
